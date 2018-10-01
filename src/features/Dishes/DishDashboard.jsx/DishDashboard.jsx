37d92935e063daf00c787841d594cf1a8441bc2d
Pururava <pururavaacharya@gmail.com>
2018-10-01 10:02:10 +0530

    DishesList config

---
+++ src/features/Dishes/DishDashboard.jsx/DishDashboard.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Grid, Button } from 'semantic-ui-react';
+import DishesList from '../DishesList/DishesList';
+//import EventForm from '../EventForm/EventForm';
+
+const DishDashboard = [
+  {
+    id: '1',
+    title: 'Paav Bhaaji',
+    
+    category: 'Indian',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: "Tower of London, St Katharine's & Wapping, London",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+
+  },
+  {
+    id: '2',
+    title: 'noodles',    
+    category: 'Chineese',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+    
+  }
+];
+
+class DishDashboard extends Component {
+
+
+
+  render() {
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <DishesList Dishes={this.state.DishDashboard} />
+        </Grid.Column>
+
+      </Grid>
+    );
+  }
+}
+
+export default DishDashboard;
--- src/features/Dishes/DishesList/DishesListItem.jsx
+++ src/features/Dishes/DishesList/DishesListItem.jsx
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import {Segment,Item} from 'semantic-ui-react';
+import { Segment, Item, Button } from 'semantic-ui-react';
 class DishesListItem extends Component {
   render() {
-      const {dishes} = this.props;
+    const {dishes} = this.props;
     return (
-        //<h1>dishes go here</h1>
-        <Segment.Group>
-            <Segment>
-                <Item.Group>
-                    <Item>
-                        <Item.Image circular src={dishes.photoURL} />
-                        <Item.Content>
-                            <Item.Header as='a'>{dishes.title} </Item.Header>
-
-                            
-                        </Item.Content>                       
-
-                    </Item>
-                </Item.Group>
-            </Segment>
-        </Segment.Group>
-      
-    )
+      <Segment.Group>
+        <Segment>
+          <Item.Group>
+            <Item>
+              <Item.Image size="tiny" circular src={dishes.hostPhotoURL} />
+              <Item.Content>
+                <Item.Header as="a">{dishes.title}</Item.Header>
+                <Item.Description>
+                  Hosted by <a>{dishes.hostedBy}</a>
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+ 
+        <Segment clearing>
+        <span>{dishes.description}</span>
+          <Button as="a" color="teal" floated="right" content="View" />
+        </Segment>
+      </Segment.Group>
+    );
   }
 }
+
 export default DishesListItem;
